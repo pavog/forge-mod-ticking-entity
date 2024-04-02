9c2f65fd6cefe2a5d5aae492b6b426857df90372
Paul Vogel <paul@aternos.org>
2024-04-02 17:37:55 +0200

    Update mod to 1.14.4

--- src/main/java/me/paulvogel/tickingentity/CentaurEntity.java
+++ src/main/java/me/paulvogel/tickingentity/CentaurEntity.java
@@ -1,23 +1,33 @@
 package me.paulvogel.tickingentity;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
-import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.network.IPacket;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
+
+import static me.paulvogel.tickingentity.Main.MODID;
 
 public class CentaurEntity extends Entity {
 
     public static String ENTITY_CENTAUR_NAME = "centaur";
 
-    public static final EntityType<CentaurEntity> CENTAUR_ENTITY_TYPE = EntityType
-            .register(Main.MODID + ":" + CentaurEntity.ENTITY_CENTAUR_NAME,
-                    EntityType.Builder
-                            .create(CentaurEntity.class, CentaurEntity::new)
-                            .tracker(256, 20, false)
-            );
+    public static final EntityType<CentaurEntity> CENTAUR_ENTITY_TYPE = (EntityType<CentaurEntity>) EntityType
+            .Builder
+            .<CentaurEntity>create(CentaurEntity::new, EntityClassification.MISC)
+            .setCustomClientFactory((spawnEntity, world) -> new CentaurEntity(world))
+            .build(MODID + ":" + CentaurEntity.ENTITY_CENTAUR_NAME)
+            .setRegistryName(new ResourceLocation(MODID, CentaurEntity.ENTITY_CENTAUR_NAME));
+
 
     public CentaurEntity(World world) {
         super(CentaurEntity.CENTAUR_ENTITY_TYPE, world);
+    }
+
+    public CentaurEntity(EntityType<CentaurEntity> type, World world) {
+        super(type, world);
     }
 
     @Override
@@ -33,12 +43,17 @@ public class CentaurEntity extends Entity {
     }
 
     @Override
-    protected void readAdditional(NBTTagCompound compound) {
+    protected void readAdditional(CompoundNBT compoundNBT) {
 
     }
 
     @Override
-    protected void writeAdditional(NBTTagCompound compound) {
+    protected void writeAdditional(CompoundNBT compoundNBT) {
 
+    }
+
+    @Override
+    public IPacket<?> createSpawnPacket() {
+        return null;
     }
 }
